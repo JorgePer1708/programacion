5deebbf5f4c1f0b1f18cdc2815095c71f51bbe7f
JorgePer1708 <jorginhio2007@gmail.com>
2025-04-16 09:22:48 -0300

    Add files via upload
    primera entrega

---
+++ pepe.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+def main () :
+  global ventana
+  #Crear ventana
+  ventana = tk.Tk()
+  ventana.geometry("300x150")
+  vetana.title("Menú Centrado")
+  vetana.mainloop()
+  
