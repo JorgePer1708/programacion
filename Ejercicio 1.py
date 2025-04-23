0c0f8abfebb3c9ae2b142f0e8bcccc0339192492
JorgePer1708 <jorginhio2007@gmail.com>
2025-04-23 09:01:10 -0300

    Add files via upload

---
+++ Ejercicio 1.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+def main() :
+  #Crear ventana
+  ventana = tk.Tk()
+  ventana.geometry("300x200")
+  ventana.title("Ejercicio 1")
+  ventana.configure(bg="red")
+  ventana.mainloop()
+  
+if __name__=="__main__":
+    main()
