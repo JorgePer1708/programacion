90c5e7d73b0ff7c0e0b96553188cce0f8945288e
JorgePer1708 <jorginhio2007@gmail.com>
2025-05-07 08:42:45 -0300

    Ejercicio 2.py

--- Ejercicio 2.py
+++ Ejercicio 2.py
@@ -3,12 +3,13 @@ import tkinter as tk
 def main() :
   #Crear la ventana principal
   ventana = tk.Tk()
-  ventana.geometry("400x200")
-  ventana.title("Ejemplo de etiqueta")
+  ventana.geometry("700x700")
+  ventana.title("ejercicio 2")
+  ventana.configure(bg="red")
  
 
   # Crear la etiqueta
-  etiqueta = tk.Label(ventana, text="Texto de la etiqueta", font=("Arial", 16))
+  etiqueta = tk.Label(ventana, text="Ejercicio 2", font=("Arial", 16))
   etiqueta.pack() # Coloca la etiqueta en la vetana
 
   #ejecutar la ventana
