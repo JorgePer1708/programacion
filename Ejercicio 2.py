00ca6aedb635e152e587d2707d2a447ac6fd44cc
JorgePer1708 <jorginhio2007@gmail.com>
2025-05-07 08:45:32 -0300

    Ejercicio 2.py

--- Ejercicio 2.py
+++ Ejercicio 2.py
@@ -10,9 +10,11 @@ def main() :
 
   # Crear la etiqueta
   etiqueta = tk.Label(ventana, text="Ejercicio 2", font=("Arial", 16))
-  etiqueta.pack() # Coloca la etiqueta en la vetana
+  etiqueta = tk.Label(ventana, text="Jorge", font=("Arial", 16))
+  etiqueta.pack() # Coloca la etiqueta en la ventana
 
   #ejecutar la ventana
   ventana.mainloop()
 if __name__=="__main__":
     main()
+
