247a9d41c3c108bb8d0ca54470093a221380c61f
JorgePer1708 <jorginhio2007@gmail.com>
2025-05-22 15:11:23 -0300

    Add files via upload

--- pepe.py
+++ pepe.py
@@ -1,12 +1,239 @@
-import tkinter as tk
+import tkinter as tk  # Tkinter para la ventana
+from urllib.request import urlopen  # Para descargar la imagen desde internet
+from PIL import Image, ImageTk  # Pillow para trabajar con imágenes
+from io import BytesIO  # Para convertir los datos de la imagen
 
-def main() :
-  global ventana
-  #Crear ventana
-  ventana = tk.Tk()
-  ventana.geometry("300x150")
-  vetana.title("Menú Centrado")
-  vetana.mainloop()
-  
+# Paso 1: Descargar la imagen de internet
+def descargarPortada():
+    urlImagen = "https://resizer.glanacion.com/resizer/v2/leonel-messi-y-cristiano-ronaldo-se-enfrentaron-QKJ62QWHIJGDNMRFMP6OW6CYB4.webp?auth=aae5b086efca8ce713dee511962b677dc47fb7feaa3ae298d46968497dcfe5ac&width=1280&height=854&quality=70&smart=true"
+    datosImagen = urlopen(urlImagen)  # Descargar la imagen
+    imagenBinaria = datosImagen.read()  # Obtener los datos de la imagen
+    # Paso 2: Convertir los datos binarios en una imagen que podamos mostrar
+    imagen = Image.open(BytesIO(imagenBinaria))
+    return imagen
+# Paso 1: Descargar la imagen de internet
+def descargarFondo():
+    urlImagen = "https://resizer.glanacion.com/resizer/v2/leonel-messi-y-cristiano-ronaldo-se-enfrentaron-QKJ62QWHIJGDNMRFMP6OW6CYB4.webp?auth=aae5b086efca8ce713dee511962b677dc47fb7feaa3ae298d46968497dcfe5ac&width=1280&height=854&quality=70&smart=true"
+    datosImagen = urlopen(urlImagen)  # Descargar la imagen
+    imagenBinaria = datosImagen.read()  # Obtener los datos de la imagen
+    # Paso 2: Convertir los datos binarios en una imagen que podamos mostrar
+    imagen = Image.open(BytesIO(imagenBinaria))
+    return imagen
+# Paso 1: Descargar la imagen de internet
+def descargarBarcelona():
+    urlImagen = "https://resizer.glanacion.com/resizer/v2/leonel-messi-y-cristiano-ronaldo-se-enfrentaron-QKJ62QWHIJGDNMRFMP6OW6CYB4.webp?auth=aae5b086efca8ce713dee511962b677dc47fb7feaa3ae298d46968497dcfe5ac&width=1280&height=854&quality=70&smart=true"
+    datosImagen = urlopen(urlImagen)  # Descargar la imagen
+    imagenBinaria = datosImagen.read()  # Obtener los datos de la imagen
+    # Paso 2: Convertir los datos binarios en una imagen que podamos mostrar
+    imagen = Image.open(BytesIO(imagenBinaria))
+    return imagen
+
+# Paso 1: Descargar la imagen de internet
+def descargarPsg():
+    urlImagen = "https://resizer.glanacion.com/resizer/v2/leonel-messi-y-cristiano-ronaldo-se-enfrentaron-QKJ62QWHIJGDNMRFMP6OW6CYB4.webp?auth=aae5b086efca8ce713dee511962b677dc47fb7feaa3ae298d46968497dcfe5ac&width=1280&height=854&quality=70&smart=true"
+    datosImagen = urlopen(urlImagen)  # Descargar la imagen
+    imagenBinaria = datosImagen.read()  # Obtener los datos de la imagen
+    # Paso 2: Convertir los datos binarios en una imagen que podamos mostrar
+    imagen = Image.open(BytesIO(imagenBinaria))
+    return imagen
+
+# Paso 1: Descargar la imagen de internet
+def descargarInterMiami():
+    urlImagen = "https://resizer.glanacion.com/resizer/v2/leonel-messi-y-cristiano-ronaldo-se-enfrentaron-QKJ62QWHIJGDNMRFMP6OW6CYB4.webp?auth=aae5b086efca8ce713dee511962b677dc47fb7feaa3ae298d46968497dcfe5ac&width=1280&height=854&quality=70&smart=true"
+    datosImagen = urlopen(urlImagen)  # Descargar la imagen
+    imagenBinaria = datosImagen.read()  # Obtener los datos de la imagen
+    # Paso 2: Convertir los datos binarios en una imagen que podamos mostrar
+    imagen = Image.open(BytesIO(imagenBinaria))
+    return imagen
+
+# Paso 1: Descargar la imagen de internet
+def descargarSeleccionArgentina():
+    urlImagen = "https://resizer.glanacion.com/resizer/v2/leonel-messi-y-cristiano-ronaldo-se-enfrentaron-QKJ62QWHIJGDNMRFMP6OW6CYB4.webp?auth=aae5b086efca8ce713dee511962b677dc47fb7feaa3ae298d46968497dcfe5ac&width=1280&height=854&quality=70&smart=true"
+    datosImagen = urlopen(urlImagen)  # Descargar la imagen
+    imagenBinaria = datosImagen.read()  # Obtener los datos de la imagen
+    # Paso 2: Convertir los datos binarios en una imagen que podamos mostrar
+    imagen = Image.open(BytesIO(imagenBinaria))
+    return imagen
+
+# Paso 1: Descargar la imagen de internet
+def descargarSportingLisboa():
+    urlImagen = "https://resizer.glanacion.com/resizer/v2/leonel-messi-y-cristiano-ronaldo-se-enfrentaron-QKJ62QWHIJGDNMRFMP6OW6CYB4.webp?auth=aae5b086efca8ce713dee511962b677dc47fb7feaa3ae298d46968497dcfe5ac&width=1280&height=854&quality=70&smart=true"
+    datosImagen = urlopen(urlImagen)  # Descargar la imagen
+    imagenBinaria = datosImagen.read()  # Obtener los datos de la imagen
+    # Paso 2: Convertir los datos binarios en una imagen que podamos mostrar
+    imagen = Image.open(BytesIO(imagenBinaria))
+    return imagen
+
+# Paso 1: Descargar la imagen de internet
+def descargarManchesterUnited1():
+    urlImagen = "https://resizer.glanacion.com/resizer/v2/leonel-messi-y-cristiano-ronaldo-se-enfrentaron-QKJ62QWHIJGDNMRFMP6OW6CYB4.webp?auth=aae5b086efca8ce713dee511962b677dc47fb7feaa3ae298d46968497dcfe5ac&width=1280&height=854&quality=70&smart=true"
+    datosImagen = urlopen(urlImagen)  # Descargar la imagen
+    imagenBinaria = datosImagen.read()  # Obtener los datos de la imagen
+    # Paso 2: Convertir los datos binarios en una imagen que podamos mostrar
+    imagen = Image.open(BytesIO(imagenBinaria))
+    return imagen
+
+# Paso 1: Descargar la imagen de internet
+def descargarRealMadrid():
+    urlImagen = "https://resizer.glanacion.com/resizer/v2/leonel-messi-y-cristiano-ronaldo-se-enfrentaron-QKJ62QWHIJGDNMRFMP6OW6CYB4.webp?auth=aae5b086efca8ce713dee511962b677dc47fb7feaa3ae298d46968497dcfe5ac&width=1280&height=854&quality=70&smart=true"
+    datosImagen = urlopen(urlImagen)  # Descargar la imagen
+    imagenBinaria = datosImagen.read()  # Obtener los datos de la imagen
+    # Paso 2: Convertir los datos binarios en una imagen que podamos mostrar
+    imagen = Image.open(BytesIO(imagenBinaria))
+    return imagen
+
+# Paso 1: Descargar la imagen de internet
+def descargarJuventus():
+    urlImagen = "https://resizer.glanacion.com/resizer/v2/leonel-messi-y-cristiano-ronaldo-se-enfrentaron-QKJ62QWHIJGDNMRFMP6OW6CYB4.webp?auth=aae5b086efca8ce713dee511962b677dc47fb7feaa3ae298d46968497dcfe5ac&width=1280&height=854&quality=70&smart=true"
+    datosImagen = urlopen(urlImagen)  # Descargar la imagen
+    imagenBinaria = datosImagen.read()  # Obtener los datos de la imagen
+    # Paso 2: Convertir los datos binarios en una imagen que podamos mostrar
+    imagen = Image.open(BytesIO(imagenBinaria))
+    return imagen
+
+# Paso 1: Descargar la imagen de internet
+def descargarUnited2():
+    urlImagen = "https://c.files.bbci.co.uk/83DA/production/_127745733_gettyimages-1242226129.jpg"
+    datosImagen = urlopen(urlImagen)  # Descargar la imagen
+    imagenBinaria = datosImagen.read()  # Obtener los datos de la imagen
+    # Paso 2: Convertir los datos binarios en una imagen que podamos mostrar
+    imagen = Image.open(BytesIO(imagenBinaria))
+    return imagen
+
+# Paso 1: Descargar la imagen de internet
+def descargarAllNassr():
+    urlImagen = "https://resizer.glanacion.com/resizer/v2/leonel-messi-y-cristiano-ronaldo-se-enfrentaron-QKJ62QWHIJGDNMRFMP6OW6CYB4.webp?auth=aae5b086efca8ce713dee511962b677dc47fb7feaa3ae298d46968497dcfe5ac&width=1280&height=854&quality=70&smart=true"
+    datosImagen = urlopen(urlImagen)  # Descargar la imagen
+    imagenBinaria = datosImagen.read()  # Obtener los datos de la imagen
+    # Paso 2: Convertir los datos binarios en una imagen que podamos mostrar
+    imagen = Image.open(BytesIO(imagenBinaria))
+    return imagen
+
+# Paso 1: Descargar la imagen de internet
+def descargarSeleccionPortugal():
+    urlImagen = "https://resizer.glanacion.com/resizer/v2/leonel-messi-y-cristiano-ronaldo-se-enfrentaron-QKJ62QWHIJGDNMRFMP6OW6CYB4.webp?auth=aae5b086efca8ce713dee511962b677dc47fb7feaa3ae298d46968497dcfe5ac&width=1280&height=854&quality=70&smart=true"
+    datosImagen = urlopen(urlImagen)  # Descargar la imagen
+    imagenBinaria = datosImagen.read()  # Obtener los datos de la imagen
+    # Paso 2: Convertir los datos binarios en una imagen que podamos mostrar
+    imagen = Image.open(BytesIO(imagenBinaria))
+    return imagen
+
+
+
+def mostrarOpcion1():
+    limpiarVentana()
+    label = tk.Label(ventana, text="Lionel Andres Messi Cuccitini", font=("Arial", 16))
+    label.pack(pady=20)
+
+    boton1 = tk.Button(ventana, text="Barcelona", font=("Arial", 12), command=mostrarMenu)
+    boton1.pack(pady=10)
+
+    boton_volver = tk.Button(ventana, text="Volver", font=("Arial", 12), command=mostrarMenu)
+    boton_volver.pack(pady=10)
+
+def mostrarOpcion2():
+    limpiarVentana()
+    label = tk.Label(ventana, text="Cristiano Ronaldo Dos Santos Aveiro", font=("Arial", 16))
+    label.pack(pady=20)
+
+    boton_volver = tk.Button(ventana, text="Volver", font=("Arial", 12), command=mostrarMenu)
+    boton_volver.pack(pady=10)
+
+def limpiarVentana():  
+    # Recorremos todos los widgets (elementos gráficos) que están dentro de la ventana
+    for widget in ventana.winfo_children():  
+        # Eliminamos cada widget para limpiar la ventana completamente
+        widget.destroy()
+
+
+
+
+        
+def mostrarMenu():
+    limpiarVentana()
+    label = tk.Label(ventana, text="Los mejores del siglo XXI", font=("Arial", 18, "bold"))
+    label.pack(pady=20)
+
+    boton1 = tk.Button(ventana, text="Messi", font=("Arial", 14), width=20, command=mostrarOpcion1)
+    boton1.pack(pady=10)
+
+    boton2 = tk.Button(ventana, text="Cristiano", font=("Arial", 14), width=20, command=mostrarOpcion2)
+    boton2.pack(pady=10)
+
+def main():
+    global ventana
+    # Crear la ventana principal
+    ventana = tk.Tk()
+    ancho = ventana.winfo_screenwidth()  # Obtengo el ancho del escritorio
+    alto = ventana.winfo_screenheight()  # Obtengo el alto del escritorio
+    ventana.geometry(f"{ancho}x{alto}")
+    ventana.title("Los Mejores del Siglo XXI")
+
+
+    imagen=descargarPortada()
+    imagenRedimensionada = imagen.resize((ancho, alto), Image.Resampling.LANCZOS)
+    portadaTk = ImageTk.PhotoImage(imagenRedimensionada)
+    ventana.portada=portadaTk
+    etiqueta = tk.Label(ventana, image=ventana.portada)
+    etiqueta.place(width=ancho, height=alto)  # Colocar la etiqueta en la ventana
+
+    imagen=descargarBarcelona()
+    imagenRedimensionada = imagen.resize((ancho, alto), Image.Resampling.LANCZOS)
+    BarcelonaTk = ImageTk.PhotoImage(imagenRedimensionada)
+    ventana.Barcelona=BarcelonaTk
+
+    imagen=descargarPsg()
+    imagenRedimensionada = imagen.resize((ancho, alto), Image.Resampling.LANCZOS)
+    PsgTk = ImageTk.PhotoImage(imagenRedimensionada)
+    ventana.Psg=PsgTk
+
+    imagen=descargarInterMiami()
+    imagenRedimensionada = imagen.resize((ancho, alto), Image.Resampling.LANCZOS)
+    InterMiamiTk = ImageTk.PhotoImage(imagenRedimensionada)
+    ventana.InterMiami=InterMiamiTk
+
+    
+    imagen=descargarSeleccionArgentina()
+    imagenRedimensionada = imagen.resize((ancho, alto), Image.Resampling.LANCZOS)
+    SeleccionArgentinaTk = ImageTk.PhotoImage(imagenRedimensionada)
+    ventana.SeleccionArgentina=SeleccionArgentinaTk
+
+    imagen=descargarSportingLisboa()
+    imagenRedimensionada = imagen.resize((ancho, alto), Image.Resampling.LANCZOS)
+    SportingLisboaTk = ImageTk.PhotoImage(imagenRedimensionada)
+    ventana.SportingLisboa=SportingLisboaTk
+    
+    imagen=descargarManchesterUnited1()
+    imagenRedimensionada = imagen.resize((ancho, alto), Image.Resampling.LANCZOS)
+    ManchesterUnited1Tk = ImageTk.PhotoImage(imagenRedimensionada)
+    ventana.ManchesterUnited1=ManchesterUnited1Tk
+
+    imagen=descargarRealMadrid()
+    imagenRedimensionada = imagen.resize((ancho, alto), Image.Resampling.LANCZOS)
+    RealMadridTk = ImageTk.PhotoImage(imagenRedimensionada)
+    ventana.RealMadrid=RealMadridTk
+
+    imagen=descargarJuventus()
+    imagenRedimensionada = imagen.resize((ancho, alto), Image.Resampling.LANCZOS)
+    JuventusTk = ImageTk.PhotoImage(imagenRedimensionada)
+    ventana.Juventus=JuventusTk
+
+    imagen=descargarUnited2()
+    imagenRedimensionada = imagen.resize((ancho, alto), Image.Resampling.LANCZOS)
+    United2Tk = ImageTk.PhotoImage(imagenRedimensionada)
+    ventana.United2=United2Tk
+
+    imagen=descargarAllNassr()
+    imagenRedimensionada = imagen.resize((ancho, alto), Image.Resampling.LANCZOS)
+    AllNassrTk = ImageTk.PhotoImage(imagenRedimensionada)
+    ventana.AllNassr=AllNassrTk
+
+    
+    
+    boton1 = tk.Button(ventana, text="Comenzar", font=("Arial", 14), width=20, command=mostrarMenu)
+    boton1.pack(pady=10)
+
+    ventana.mainloop()
+
 if __name__=="__main__":
     main()
